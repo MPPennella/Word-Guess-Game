07d7aa7af1f31e9cb50e1d8f6299d7c12bef9cee
M. Pennella <michael.pennella@gmail.com>
2018-08-24 03:50:18 -0700

    Created state functionality to distinguish between-game behavior and main gameplay behavior

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,7 @@ var game = {
     ],
 
     // Values for holding important game information
+    state: "interGame",
     chosenWord: "",
     wordField: "",
     wrongGuesses: [],
@@ -41,56 +42,64 @@ var game = {
             
         }
 
-        // Checks if key was alphabetic A-Z key (keyCode range 65 to 90)
-        if (65<=keyCode && keyCode<=90) {
-            // Convert keycode to string representation of letter
-            let letter = String.fromCharCode(keyCode);
+        if (this.state == "interGame") {
+            this.newGame();
+        }
 
-            // Check if letter has been guessed before - only execute if new guess
-            if ( !this.letterGuessed(letter) ) {
-                // Check if keypress is letter in word
-                if (this.chosenWord.includes(letter)) {
-                    console.log("MATCH");
+        // Only run this if a word is actively being guessed
+        if (this.state == "activeGame") {
+            // Checks if key was alphabetic A-Z key (keyCode range 65 to 90)
+            if (65<=keyCode && keyCode<=90) {
+                // Convert keycode to string representation of letter
+                let letter = String.fromCharCode(keyCode);
 
-                    // Find position of all matches in chosenWord and insert letter at matching indices of wordField
-                    for (let i=0; i<this.chosenWord.length; ++i) {
-                        if (this.chosenWord.charAt(i) == letter) {
-                            // Cut off substrings before and after index and substitute letter in between
-                            this.wordField = this.wordField.substring(0,i) + letter + this.wordField.substring(i+1);
+                // Check if letter has been guessed before - only execute if new guess
+                if ( !this.letterGuessed(letter) ) {
+                    // Check if keypress is letter in word
+                    if (this.chosenWord.includes(letter)) {
+                        console.log("MATCH");
+
+                        // Find position of all matches in chosenWord and insert letter at matching indices of wordField
+                        for (let i=0; i<this.chosenWord.length; ++i) {
+                            if (this.chosenWord.charAt(i) == letter) {
+                                // Cut off substrings before and after index and substitute letter in between
+                                this.wordField = this.wordField.substring(0,i) + letter + this.wordField.substring(i+1);
+                            }
+                        }
+                        
+                        // If entire word is guessed, VICTORY
+                        if (this.chosenWord == this.wordField) {
+                            // VICTORY
+                            console.log("YOU WON");
+                        }
+
+                    } else {
+                        console.log("NO MATCH");
+
+                        // Add letter to list of wrong guesses
+                        this.wrongGuesses.push(letter);
+
+                        // Decrement number of remaining guesses by 1
+                        this.guessesRemaining--;
+
+                        // If no guesses left, DEFEAT
+                        if (this.guessesRemaining == 0) {
+                            // DEFEAT
+                            console.log("YOU LOST");
                         }
                     }
-                    
-                    // If entire word is guessed, VICTORY
-                    if (this.chosenWord == this.wordField) {
-                        // VICTORY
-                        console.log("YOU WON");
-                    }
+                } 
 
-                } else {
-                    console.log("NO MATCH");
-
-                    // Add letter to list of wrong guesses
-                    this.wrongGuesses.push(letter);
-
-                    // Decrement number of remaining guesses by 1
-                    this.guessesRemaining--;
-
-                    // If no guesses left, DEFEAT
-                    if (this.guessesRemaining == 0) {
-                        // DEFEAT
-                        console.log("YOU LOST");
-                    }
-                }
-            } 
-
-            // Push value changes to screen
-            this.updateGameInfo();
-
+                // Push value changes to screen
+                this.updateGameInfo();
+            }
         } 
     },
 
     // Chooses a new word to be guessed and reinitializes game parameters to starting values
     newGame() {
+        // Change instructions
+        document.getElementById("instructions").textContent = "Press any letter key to guess that letter. If you guess wrong too many times, you lose. If you can complete the word before running out of guesses, you win!"
 
         // Choose a new word to be guessed
         this.chooseWord();
@@ -109,6 +118,12 @@ var game = {
         // Reset guessesRemaining counter
         this.guessesRemaining = 10;
         console.log(this.guessesRemaining);
+
+        // Update screen with intital game values
+        this.updateGameInfo();
+        
+        // Set game state to active
+        this.state = "activeGame";
     
     },
 
